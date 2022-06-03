6c231519201da0470d8f9c0a7bcb246be3403728
LDeepakReddy <l.deepakreddy.com>
2022-06-04 01:14:29 +0530

    Added : Functionality to calculate PartTime employee wage per day

--- EmpWage.php
+++ EmpWage.php
@@ -1,18 +1,26 @@
 <?php
 class EmpWage {
-    public $WAGE_PER_HR = 20;
-    public $FULL_TIME_WORKING_HRS = 8;
+    public $WAGE_PER_HOUR = 20;
+    public $FULL_TIME_WORKING_HOURS = 8;
+    public $PART_TIME_WORKING_HOURS = 4;
     
 
 // function to check employee is present or absent
-    function empAttendance(){   
-        $empCheck = rand(0,1);
-        if ($empCheck == 1) {
-            echo "Employee is present\n";
-            
-            $dailyWage =  $this-> WAGE_PER_HR *  $this-> FULL_TIME_WORKING_HRS ; //Daily wage calculation 
-            echo "Employee Daily Wage is :  $dailyWage ";        
-        }else{
+    function empAttendance()
+    {   
+        $empCheck = rand(0,2);
+        if ($empCheck == 2) {
+            echo "Employee is present and Full time Employee\n";
+            $dailyWage =  $this-> WAGE_PER_HOUR *  $this-> FULL_TIME_WORKING_HOURS ; //Daily wage calculation for fulltime employee
+            echo "Employee Daily Wage is :  $dailyWage ";       
+
+        }elseif($empCheck == 1){
+            echo "Employee is present and Part time Employee\n";
+            $dailyWage = $this->WAGE_PER_HOUR * $this->PART_TIME_WORKING_HOURS; //Daily wage calculation for Partime employee
+            echo "Employee Daily Wage is : $dailyWage ";
+        }
+        
+        else{
             echo "Employee is absent";
         }
 
