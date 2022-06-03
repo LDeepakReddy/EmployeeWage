2c3710b9afef65316da4a51267cc818cfddcac85
LDeepakReddy <l.deepakreddy.com>
2022-06-04 00:28:45 +0530

    Added : Function to check the employee is present or absent

--- EmpWage.php
+++ EmpWage.php
@@ -1,9 +1,25 @@
 <?php
-class EmpWage{
+class EmpWage {
     static function welcome(){
         echo "Welcome to employee wage program";
     }
+
+// function to check employee is present or absent
+    function empAttendance(){   
+        $empCheck = rand(0,1);
+        if ($empCheck == 1) {
+            echo "Employee is present";
+        }else{
+            echo "Employee is absent";
+        }
+
+    }
 }
-EmpWage ::welcome()
+
+// object creation of class
+$employeeWage = new EmpWage();
+$employeeWage -> empAttendance();
+
+
 
 ?>
