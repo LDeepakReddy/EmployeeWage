ee8e0edbb17d1eacbaf19ae740253d65869239f1
LDeepakReddy <l.deepakreddy.com>
2022-06-04 00:55:07 +0530

    Added : Functionality to check the employee daily wage if he is present

--- EmpWage.php
+++ EmpWage.php
@@ -1,24 +1,28 @@
 <?php
 class EmpWage {
-    static function welcome(){
-        echo "Welcome to employee wage program";
-    }
+    public $WAGE_PER_HR = 20;
+    public $FULL_TIME_WORKING_HRS = 8;
+    
 
 // function to check employee is present or absent
     function empAttendance(){   
         $empCheck = rand(0,1);
         if ($empCheck == 1) {
-            echo "Employee is present";
+            echo "Employee is present\n";
+            
+            $dailyWage =  $this-> WAGE_PER_HR *  $this-> FULL_TIME_WORKING_HRS ; //Daily wage calculation 
+            echo "Employee Daily Wage is :  $dailyWage ";        
         }else{
             echo "Employee is absent";
         }
 
     }
+
 }
 
 // object creation of class
 $employeeWage = new EmpWage();
-$employeeWage -> empAttendance();
+$employeeWage->empAttendance();
 
 
 
