7297d732b2b9384c588f2c7c15593ce95ae7a3f4
LDeepakReddy <l.deepakreddy.com>
2022-06-04 01:30:35 +0530

    Modified : Program to make use of the switch case statements

--- EmpWage.php
+++ EmpWage.php
@@ -1,37 +1,35 @@
 <?php
-class EmpWage {
+class EmpWage
+{
     public $WAGE_PER_HOUR = 20;
     public $FULL_TIME_WORKING_HOURS = 8;
     public $PART_TIME_WORKING_HOURS = 4;
-    
 
-// function to check employee is present or absent
+
+    // function to check employee is present or absent
     function empAttendance()
-    {   
-        $empCheck = rand(0,2);
-        if ($empCheck == 2) {
-            echo "Employee is present and Full time Employee\n";
-            $dailyWage =  $this-> WAGE_PER_HOUR *  $this-> FULL_TIME_WORKING_HOURS ; //Daily wage calculation for fulltime employee
-            echo "Employee Daily Wage is :  $dailyWage ";       
+    {
+        $empCheck = rand(0, 2);
+        switch ($empCheck) {
+            case 1:
+                echo "Employee is present and Works as Part time Employee\n";
+                $dailyWage = $this->WAGE_PER_HOUR * $this->PART_TIME_WORKING_HOURS; //Daily wage calculation for Partime employee
+                echo "Employee Daily Wage is : $dailyWage ";
+                break;
 
-        }elseif($empCheck == 1){
-            echo "Employee is present and Part time Employee\n";
-            $dailyWage = $this->WAGE_PER_HOUR * $this->PART_TIME_WORKING_HOURS; //Daily wage calculation for Partime employee
-            echo "Employee Daily Wage is : $dailyWage ";
-        }
-        
-        else{
-            echo "Employee is absent";
-        }
+            case 2:
+                echo "Employee is present and Works as Full time Employee\n";
+                $dailyWage =  $this->WAGE_PER_HOUR *  $this->FULL_TIME_WORKING_HOURS; //Daily wage calculation for fulltime employee
+                echo "Employee Daily Wage is :  $dailyWage ";
+                break;
 
+            default:
+                echo "Employee is Absent\n";
+                break;
+        }
     }
-
 }
 
 // object creation of class
 $employeeWage = new EmpWage();
 $employeeWage->empAttendance();
-
-
-
-?>
