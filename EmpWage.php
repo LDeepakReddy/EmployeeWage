e5d7d50441a8fc8b5edf4d8858f450d2cfd6022f
LDeepakReddy <l.deepakreddy.com>
2022-06-09 10:37:38 +0530

    Added: Function to calculate wages for multiple companies by taking user input values for different companies

--- EmpWage.php
+++ EmpWage.php
@@ -1,14 +1,14 @@
 <?php
 class EmpWage
 {
-    const WAGE_PER_HOUR = 20;
+    //const WAGE_PER_HOUR = 20;
     const FULL_TIME_WORKING_HOURS = 8;
     const PART_TIME_WORKING_HOURS = 4;
     const IS_FULL_TIME = 2;
     const IS_PART_TIME = 1;
     const IS_ABSENT = 0;
-    const WORKING_DAYS_PER_MONTH = 20;
-    const WORKING_HOURS_PER_MONTH = 100;
+    //const WORKING_DAYS_PER_MONTH = 20;
+   // const WORKING_HOURS_PER_MONTH = 100;
 
    public $workingHours = 0;
    public $monthlyWage= 0;
@@ -38,15 +38,25 @@ class EmpWage
                 break;
         }
     }
+    
+    function userInput(){
+        $company = readline("Enter the name of Company : ");
+        echo "Wage for $company Employee \n ";
+        $wagePerHour = readline("Enter the wage per hour : ");
+        $workingDaysPerMonth = readline("Enter working Days per month : ");
+        $workingHoursPerMonth = readline ("ENter total working hours per month : ");
+        $this->monthlyWage($wagePerHour,$workingDaysPerMonth,$workingHoursPerMonth);
+
+        
+    }
 
   
     // Function to Calculate Daily Wage
-    function dailyWage()
+    function dailyWage($wagePerHour)
     {
-        $totalHrs = $this->empAttendance();
-        $this->workingHours = $totalHrs;
-        $dailyWage = EmpWage ::WAGE_PER_HOUR * $totalHrs;
-        echo "Total Working Hours : " . $totalHrs . "\n";
+        $this->workingHours =  $this->empAttendance();
+        $dailyWage = $wagePerHour * $this-> workingHours;
+        echo "Total Working Hours : " . $this-> workingHours . "\n";
         echo "Daily Wage : " . $dailyWage . "\n\n";
         return $dailyWage;
     }
@@ -55,23 +65,25 @@ class EmpWage
     //Function to Calculate Monthly Wage
     //calculating monthly wage according to working hours
     //calling dailyWage() function to get daily wage
-    function monthlyWage()
+    function monthlyWage($wagePerHour,$workingDaysPerMonth,$workingHoursPerMonth)
     {
-        while($this->totalWorkingHours <= EmpWage::WORKING_HOURS_PER_MONTH &&
-            $this->totalWorkingDays < EmpWage ::WORKING_DAYS_PER_MONTH
+        while($this->totalWorkingHours <= $workingHoursPerMonth &&
+            $this->totalWorkingDays < $workingDaysPerMonth
         ){
             $this->totalWorkingDays++;
             echo "Day : " . $this->totalWorkingDays. "\n";
-            $dailyWage = $this->dailyWage();
+            $dailyWage = $this->dailyWage($wagePerHour);
             $this->monthlyWage += $dailyWage;
             $this->totalWorkingHours += $this->workingHours;
+                $this->totalWorkingDays++;
         }
        echo "Total Working Days : $this->totalWorkingDays\n";
        echo "Total Working Hours : $this->totalWorkingHours\n";
        
         echo "Total Monthly Wage : $this->monthlyWage \n\n";
     }
+    
 }
 //Creating object
 $empWage = new EmpWage();
-$empWage->monthlyWage();
+$empWage->userInput();
